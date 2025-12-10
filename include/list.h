89a8ec64be0db8bd35c68afff98c459216a770de
ivan gnomov <sobakpes94@ya.ru>
2025-12-10 10:44:07 +0300

    Rewritten list

--- include/list.h
+++ include/list.h
@@ -1,16 +1,23 @@
 #ifndef LIST
 #define LIST
 
-typedef struct node
+struct node
 {
   char *name;
   void *value;
   struct node *next;
+};
+
+typedef struct list
+{
+  struct node *head;
+  void (*insert) (struct list *self, const char *name, void *value);
+  void (*remove) (struct list *self, const char *name);
+  void * (*search) (struct list *self, const char *name);
 } list_t;
 
-list_t *list_insert (list_t *head, const char *name, void *value);
-list_t *list_delete (list_t *head, const char *name);
-list_t *list_search (list_t *head, const char *name);
-void list_destroy (list_t *head);
+void list_init (list_t *self);
+void list_free (list_t *self);
+void list_freec (list_t *self, void (*free_func) (void *ptr));
 
 #endif
--- src/list.c
+++ src/list.c
@@ -2,24 +2,24 @@
 #include <stdlib.h>
 #include <string.h>
 
-list_t *
-list_insert (list_t *head, const char *name, void *value)
+struct node *
+node_insert (struct node *head, const char *name, void *value)
 {
   if (!head)
     {
-      list_t *new = (list_t *)malloc (sizeof (list_t));
+      struct node *new = (struct node *)malloc (sizeof (struct node));
       new->value = value;
       new->name = malloc (strlen (name));
       strcpy (new->name, name);
       new->next = NULL;
       return new;
     }
-  head->next = list_insert (head->next, name, value);
+  head->next = node_insert (head->next, name, value);
   return head;
 }
 
-list_t *
-list_delete (list_t *head, const char *name)
+struct node *
+node_remove (struct node *head, const char *name)
 {
   if (!head)
     {
@@ -27,32 +27,88 @@ list_delete (list_t *head, const char *name)
     }
   if (!strcmp (head->name, name))
     {
-      list_t *tmp = head;
+      struct node *tmp = head;
       head = head->next;
+      free (tmp->name);
       free (tmp);
       return head;
     }
-  head->next = list_delete (head->next, name);
+  head->next = node_remove (head->next, name);
   return head;
 }
 
-list_t *
-list_search (list_t *head, const char *name)
+struct node *
+node_search (struct node *head, const char *name)
 {
   if (!head)
     return head;
   if (!strcmp (head->name, name))
     return head;
-  return list_search (head->next, name);
+  return node_search (head->next, name);
 }
 
 void
-list_destroy (list_t *head)
+node_free (struct node *head)
 {
   if (head)
     {
-      list_t *tmp = head->next;
+      struct node *tmp = head->next;
+      free (head->name);
       free (head);
-      list_destroy (tmp->next);
+      node_free (tmp->next);
     }
+}
+
+void
+node_freec (struct node *head, void (*free_func) (void *ptr))
+{
+  if (head)
+    {
+      struct node *tmp = head->next;
+      free (head->name);
+      free_func (head->value);
+      free (head);
+      node_freec (tmp->next, free_func);
+    }
+}
+
+void
+list_insert (list_t *self, const char *name, void *value)
+{
+  self->head = node_insert (self->head, name, value);
+}
+
+void
+list_remove (list_t *self, const char *name)
+{
+  self->head = node_remove (self->head, name);
+}
+
+void *
+list_search (list_t *self, const char *name)
+{
+  return node_search (self->head, name)->value;
+}
+
+void
+list_init (list_t *self)
+{
+  self = malloc (sizeof (list_t));
+  self->insert = list_insert;
+  self->remove = list_remove;
+  self->search = list_search;
+}
+
+void
+list_free (list_t *self)
+{
+  node_free (self->head);
+  free (self);
+}
+
+void
+list_freec (list_t *self, void (*free_func) (void *))
+{
+  node_freec (self->head, free_func);
+  free (self);
 }
